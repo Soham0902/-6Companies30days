782e2e930465efdf29495d34c3f6072e22ecc3c6
Soham0902 <bhirangesoham1@gmail.com>
2024-12-31 20:33:52 +0530

    WhoIsTheWinner added

---
+++ Find_the_Winner.java
@@ -0,0 +1,10 @@
+// link of the problem : https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+class Find_the_Winner{
+    public int findthewinner(int n, int k){
+        int winner = 0;
+        for(int i=1;i<=n;i++){
+            winner = (winner+k)%i;
+        }
+        return winner+1;
+    }
+}
