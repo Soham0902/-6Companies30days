b467ff2e086c9a9d56ee92c34161e9c4f8c98fc3
Soham0902 <bhirangesoham1@gmail.com>
2025-01-07 11:43:14 +0530

    First Unique Character In String added

---
+++ Amazon/FirstUniqueCharacterInString.java
@@ -0,0 +1,19 @@
+package Amazon;
+
+import java.util.HashMap;
+public class FirstUniqueCharacterInString {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer>map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        int ans = -1;
+        for(int i = 0;i<s.length();i++){
+            if(map.get(s.charAt(i)) == 1){
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+}
