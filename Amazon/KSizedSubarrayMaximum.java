b865fa85a519125f5e38bef7f777ef97805e3820
Soham0902 <bhirangesoham1@gmail.com>
2025-01-09 13:35:27 +0530

    K sized subarray Maximum added

---
+++ Amazon/KSizedSubarrayMaximum.java
@@ -0,0 +1,32 @@
+package Amazon;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class KSizedSubarrayMaximum {
+    public ArrayList<Integer> max_of_subarrays(int arr[], int k) {
+        int n = arr.length;
+        ArrayList<Integer> res = new ArrayList<Integer>(0);
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        int i = 0;
+        for (i = 0; i < k; i++) {
+            while (dq.isEmpty() == false && arr[i] >= arr[dq.peekLast()]) dq.pollLast();
+
+            dq.add(i);
+        }
+
+        for (; i < n; i++) {
+
+            res.add(arr[dq.peek()]);
+
+            while (dq.isEmpty() == false && (dq.peekFirst() <= i - k)) dq.pollFirst();
+
+            while (dq.isEmpty() == false && (arr[i] >= arr[dq.peekLast()]))
+                dq.pollLast();
+            dq.add(i);
+        }
+        res.add(arr[dq.peek()]);
+        dq.pollFirst();
+        return res;
+    }
+}
