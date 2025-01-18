8bb6f92c7c4c6dccd94a7098057053e8e4d54aaa
Soham0902 <bhirangesoham1@gmail.com>
2025-01-18 13:19:27 +0530

    Stone Game 6 added

---
+++ Google/StoneGame6.java
@@ -0,0 +1,24 @@
+package Google;
+
+import java.util.Arrays;
+
+public class StoneGame6 {
+    public int stoneGameVI(int[] aliceValues, int[] bobValues) {
+        int n = aliceValues.length;
+        int [][]sums = new int [n][];
+        for(int i=0;i<n;i++){
+            sums[i] = new int[]{aliceValues[i]+bobValues[i], aliceValues[i], bobValues[i]};
+        }
+        Arrays.sort(sums,(a,b)-> Integer.compare(b[0],a[0]));
+        int a = 0;
+        int b = 0;
+        for(int i=0;i < n;i++){
+            if(i%2==0){
+                a+=sums[i][1];
+            } else{
+                b+=sums[i][2];
+            }
+        }
+        return Integer.compare(a,b);
+    }
+}
